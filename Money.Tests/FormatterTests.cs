c4d9fdda5fe162e1f7235f0aed69d80506bf3f6f
Erik Hennerfors <erik.hennerfors@laget.se>
2021-12-11 04:25:21 +0100

    Update(s) porting formater

---
+++ Money.Tests/FormatterTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Money.Tests
+{
+    public class FormatterTests
+    {
+        [Fact]
+        public void ShouldUseCorrectThousandsSeparator()
+        {
+            var usd = new Money(19.99, Currency.USD);
+            Assert.Equal(".", usd.Currency.DecimalMark);
+            Assert.Equal(",", usd.Currency.ThousandsSeparator);
+            Assert.Equal("$19.99", usd.ToString());
+
+            var sek = new Money(199.95, Currency.SEK);
+            Assert.Equal(".", sek.Currency.DecimalMark);
+            Assert.Equal(",", sek.Currency.ThousandsSeparator);
+        }
+    }
+}
---
+++ Money/Formatter.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace Money
+{
+    public class Formatter
+    {
+        private readonly Currency _currency;
+
+        public Formatter(Currency currency)
+        {
+            _currency = currency;
+        }
+
+        public string Format(Money money)
+        {
+            var result = FormatNumber(money);
+
+            return result;
+
+        }
+
+        public string FormatNumber(Money money)
+        {
+            // Format whole and decimal parts separately
+            var (decimalPart, numberPart) = ExtractParts(money);
+
+            // Format whole and decimal parts separately
+            decimalPart = FormatDecimalPart(decimalPart);
+            numberPart = FormatNumberPart(numberPart);
+
+            return $"{numberPart}{_currency.DecimalMark}{decimalPart}";
+        }
+
+        private (string, string) ExtractParts(Money money)
+        {
+            var fractional = money.Fractional;
+
+            // Translate subunits into units
+            var units = ((decimal)fractional / (decimal)_currency.SubUnitToUnit);
+
+            // Split the result and return whole and decimal parts separately
+            var values = units.ToString("F", new CultureInfo("en-US")).Split('.');
+
+            return (values[1], values[0]);
+        }
+
+        private string FormatDecimalPart(string value)
+        {
+            // Pad value, making up for missing zeroes at the end
+            value = value.ljust(_currency.Exponent, '0');
+        }
+
+        private string FormatNumberPart(string value)
+        {
+            return null;
+        }
+    }
+}
